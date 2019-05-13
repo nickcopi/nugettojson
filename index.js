e8b60e4d1eda835572b47e2b1323d96877e82003
Nick <>
2019-05-13 14:03:05 -0400

    Full local mirroring (turbo)

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const request = require('request-promise');
 const parser = require('fast-xml-parser');
 const mkdirp = require('mkdirp');
 const fs = require('fs');
+const AdmZip = require('adm-zip');
+const rimraf = require('rimraf');
 
 let getData = async () =>{
 	let data = await request('https://choco.lcc.ts.vcu.edu/nuget/LCC/Packages');
@@ -19,31 +21,56 @@ let getData = async () =>{
 		d.properties = newObject;
 	}
 	);
-
-	fs.writeFileSync('output.json',JSON.stringify(data,null,2));
-	return data.feed.entry;
+	const outputFileName = 'output.json';
+	const oldData = fs.existsSync(outputFileName)?JSON.parse(fs.readFileSync(outputFileName).toString('utf-8')):null;
+	fs.writeFileSync(outputFileName,JSON.stringify(data.feed.entry,null,2));
+	return {
+		oldData,
+		newData: data.feed.entry
+		}
 
 }
 
 let fetchPackage = async (name,version)=>{
 	const pkgName = `${name}_${version}`; 
 	const path = `packages/${pkgName}`;
+	const outputPath = path + '/output';
+	const zipPath = `${path}/${pkgName}.nupkg`;
 	mkdirp.sync(path);
+	rimraf.sync(outputPath);
+	mkdirp.sync(outputPath);
 	const options = {
 		url: `https://choco.lcc.ts.vcu.edu/nuget/LCC/package/${name}/${version}`,
 		encoding: null
 	}
 	let data = await request(options);
-	fs.writeFileSync(`${path}/${pkgName}.nupkg`,data);
+	fs.writeFileSync(zipPath,data);
+	(new AdmZip(data)).extractAllTo(outputPath,true);
+	debugger;
 }
 
 
 
 
 getData().then(res=>{
+	let newData = res.newData;
+	let oldData = res.oldData;
 	//console.log(res.map(r=>r.title).filter((r,i,arr)=>arr.indexOf(r) === i));
-	fetchPackage(res[0].title,res[0].properties.Version);
-	
+	newData.forEach(d=>{
+		const name = d.title;
+		const version = d.properties.Version
+		const path = `packages/${name}_${version}`;
+		let localExists = fs.existsSync(path);
+		let oldVersion = oldData.find(o=>o.title === name && o.properties.Version === version);
+		let hashDiffers = !oldVersion || oldVersion.properties.PackageHash !== d.properties.PackageHash;
+		if(!localExists || hashDiffers){
+			console.log(`Grabbing ${name} version ${version}.`);
+			fetchPackage(name,version);
+		}
+		debugger;
+
+	});
+
 }).catch(e=>{
 	console.error(e);
 });
