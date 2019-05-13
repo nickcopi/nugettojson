323f1225bb9538d83e575cb838f36ab92dd266b2
Nick <>
2019-05-13 10:38:21 -0400

    Epic initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const request = require('request-promise');
+const parser = require('fast-xml-parser');
+const fs = require('fs');
+
+let getData = async () =>{
+	let data = await request('https://choco.lcc.ts.vcu.edu/nuget/LCC/Packages');
+	data = parser.parse(data);
+	newData = data.feed.entry;
+	newData = newData.map(d=>{
+		d.properties = JSON.parse(JSON.stringify(d['m:properties']));
+		delete d['m:properties'];
+		const newObject = {};
+		Object.keys(d.properties).forEach(p=>{
+			newObject[p.substring(2)] = d.properties[p];
+			delete d.properties[p];
+		}
+		);
+		d.properties = newObject;
+	}
+	);
+
+	fs.writeFileSync('output.json',JSON.stringify(data,null,2));
+
+}
+console.log(Promise.resolve(getData()));
